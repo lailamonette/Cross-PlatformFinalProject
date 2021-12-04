1933d612f13a2344d3dff51136da1a03c3086003
Ahmet Rusen Karaoglu <ahmetrusenkara@gmail.com>
2021-12-04 14:23:16 -0800

    updates script.js

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ const personalPriceDetails = document.querySelector('.personal-price-details');
 const premiumPriceDetails = document.querySelector('.premium-price-details');
 const businessPriceDetails = document.querySelector('.business-price-details');
 
+const annualBenefits = document.querySelectorAll('.annual-benefits');
 
 monthlyButton.addEventListener('click', function(){
     monthlyButton.classList.add('button-clicked')
     yearlyButton.classList.remove('button-clicked');
+
+    for(x = 0; x < annualBenefits.length; x++){
+        annualBenefits[x].classList.remove('turnOn');
+        annualBenefits[x].classList.add('turnOff');
+    }
+
     personalPrice.textContent = 'CAD $10';
     premiumPrice.textContent = 'CAD $15';
     businessPrice.textContent = 'CAD $30';
@@ -24,6 +31,12 @@ monthlyButton.addEventListener('click', function(){
 yearlyButton.addEventListener('click', function(){
     monthlyButton.classList.remove('button-clicked')
     yearlyButton.classList.add('button-clicked');
+
+    for(x = 0; x < annualBenefits.length; x++){
+        annualBenefits[x].classList.remove('turnOff');
+        annualBenefits[x].classList.add('turnOn');
+    }
+
     personalPrice.textContent = 'CAD $6';
     premiumPrice.textContent = 'CAD $10';
     businessPrice.textContent = 'CAD $15';
