b037a0417ce9acd449c94a5771471bf4ad20c3e3
Ahmet Rusen Karaoglu <ahmetrusenkara@gmail.com>
2021-12-12 18:32:53 -0800

    Update layouts_Ahmet.js

--- layouts_Ahmet.js
+++ layouts_Ahmet.js
@@ -1,9 +1,9 @@
 const favButton1 = document.querySelector('.btn_favorite_1');
 const favButton2 = document.querySelector('.btn_favorite_2');
 
-favButton1.addEventListener('clicked', function(){
+favButton1.addEventListener('click', function(){
     favButton1.classList.toggle('btn_favorite_clicked');
 })
-favButton2.addEventListener('clicked', function(){
+favButton2.addEventListener('click', function(){
     favButton2.classList.toggle('btn_favorite_clicked');
-})
+});
